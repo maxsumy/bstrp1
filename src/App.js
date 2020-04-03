1260991624a69549bb433bb3aeac88bdd04c2a9b
maxsumy <grenki2@gmail.com>
2020-04-03 20:55:33 +0300

    firs commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import Test from "./components/Test";
+import Adding_new_lost from "./components/Adding_new_lost";
+import Sign from "./components/Sing";
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      {/*<Test/>*/}
+      <Adding_new_lost/>
+    </div>
+  );
+}
+
+export default App;
